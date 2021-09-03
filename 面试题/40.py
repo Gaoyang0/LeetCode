45e721b8ac8b313950f90ec436407de9af58b2f5
Gaoyang <1298635601@qq.com>
2021-09-03 22:49:13 +0800

    add

--- 面试题/40.py
+++ 面试题/40.py
@@ -3,20 +3,31 @@ from typing import List
 
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        if sum(candidates) < target:
+            return []
 
-        res = []
+        res = set()
         def bak(cur, s, index):
             if s == target:
-                res.append(cur)
+                if tuple(cur.copy()) not in res:
+                    res.add(tuple(cur.copy()))
+            if index == len(candidates):
+                return
             elif s < target:
                 for i in range(index, len(candidates)):
                     cur.append(candidates[i])
                     bak(cur, s + candidates[i], i + 1)
                     cur.pop()
-        for index, item in enumerate(candidates):
-            bak([item], item, index+1)
-        return res
+
+        candidates.sort()
+        bak([], 0, 0)
+        return [list(i) for i in res]
 
 
 s = Solution()
-print(s.combinationSum2([10,1,2,7,6,1,5], 8))
+# print(s.combinationSum2([10,1,2,7,6,1,5], 8))
+# print(s.combinationSum2([2,5,2,1,2], 5))
+# print(s.combinationSum2([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1], 27))
+print(s.combinationSum2([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+, 30))
+# print(len([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]))
--- 面试题/685.py
+++ 面试题/685.py
@@ -18,14 +18,6 @@ class Solution:
                 p = parent[p]
             return p
 
-        def get_all_father(index, parent):
-            res = {index}
-            p = index
-            while parent[p] != -1:
-                p = parent[p]
-                res.add(p)
-            return res
-
         def union_node(u, v, parent):
             u_root = find_root(u, parent)
             v_root = find_root(v, parent)
@@ -41,37 +33,40 @@ class Solution:
             node_set.add(v)
 
         # 已经有入度了
-        in_set = dict()
+        in_dict = dict()
         parent = [-1] * (len(node_set) + 1)
 
         conflict = -1
         for i in range(len(edges)):
             u, v = edges[i]
-            if v in in_set:
+            if v in in_dict:
                 # 冲突
                 conflict = v
             else:
-                in_set[v] = []
-            in_set[v].append(u)
+                in_dict[v] = []
+            in_dict[v].append(u)
 
             if not union_node(u, v, parent):
                 # 得到环
-                u_f = get_all_father(u, parent)
-                v_f = get_all_father(v, parent)
+                all_cycle_node = get_cycle_node(u, v, parent)
+
 
                 all_cycle_node = u_f | v_f
 
                 if conflict != -1:
+                    if conflict == v:
+                        return [u, v]
                     # 有环且冲突，应该删除环里的边
                     if conflict in all_cycle_node:
-                        return [u_f[conflict][0], conflict]
+                        return [in_dict[conflict][0], conflict]
 
                 for u_last, v_last in edges[i+1:]:
-                    if v_last in u_f.keys():
+                    if v_last in in_dict:
                         # 有环且冲突，应该删除环里的边
-                        return [u_f[v_last][0], v_last]
+                        return [in_dict[v_last][0], v_last]
                 # 有环，无冲突
                 return [u, v]
+        return [in_dict[conflict][-1], conflict]
 
 
 s = Solution()
@@ -82,6 +77,11 @@ s = Solution()
 # 有环无冲突
 # print(s.findRedundantDirectedConnection([[1,2],[2,3],[3,1]]))
 # 有环有冲突
-print(s.findRedundantDirectedConnection([[1,2],[2,3],[3,1],[4,2]]))
+# print(s.findRedundantDirectedConnection([[1,2],[2,3],[3,1],[4,2]]))
+# 无环有冲突
 # print(s.findRedundantDirectedConnection([[1,2],[1,3],[2,3]]))
+# print(s.findRedundantDirectedConnection([[1,2],[1,3],[4,2]]))
 # print(s.findRedundantDirectedConnection([[2,1],[3,1],[4,2],[1,4]]))
+# print(s.findRedundantDirectedConnection([[1,2],[1,3],[2,3]]))
+print(s.findRedundantDirectedConnection(
+[[5,2],[5,1],[3,1],[3,4],[3,5]]))
